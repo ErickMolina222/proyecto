99df64a822ea1de78834e2ad06b5efeefb9f1424
luis <luis__gz1@tesch.edu.mx>
2025-06-18 16:34:44 -0600

    correcciones

--- tablas.sql
+++ tablas.sql
@@ -1,29 +1,23 @@
 create table carrera(
-    id_carrera int(1),
-    nombre varchar(30),
+    id_carrera int(4),
+    nombreCa varchar(30),
 );
 
 create table productoAca(
-    id_Pa int(4),
+    id_pa int(4),
+    Tipo varchar(12),
+    Estatus varchar(20),
     titulo varchar(30),
     fecha_inicio Date,
     fecha_termino Date,
-    DocumentoProvatorio varchar(1),
-    urlConsulta varchar(200),
+    DocumentoProvatorio varchar(50),
+    urlConsulta varchar(50),
     borrado int(1),
-    id_Tipo int(4),
-    id_Estatus int(4)
+    PRIMARY KEY (id_pa)
 );
 
 create table estatus(
     id_Estatus int(4),
-    nombre varchar(50)
-    descripcion varchar(200),
-    borrado int(1)
-);
-
-create table tipo(
-    id_Tipo int(4),
     nombre varchar(50)
     descripcion varchar(200),
     borrado int(1)
